136e854667375e79a993fdb518a5668a44e4d8d1
Tran Quang Tung <tungonlytop1@gmail.com>
2023-12-26 16:08:41 +0700

    first commit

---
+++ Module1.java
@@ -0,0 +1,27 @@
+public class Module1 {
+
+    public static void printString(String a, String b){
+        System.out.println(a + " " + b);
+    }
+    public static void main(String[] args) throws Exception {
+        //Bai1
+        /*System.out.println("Hello World!");
+        */
+
+        //Bai2
+        /*String helloWorld = "Hello World!";
+        System.out.println(helloWorld);
+        */
+
+        //Bai3 
+        String hello = "Hello";
+        String world = "World";
+        String helloWorld = hello + " " + world;
+        System.out.println(helloWorld);
+        
+        //Bai4
+        printString(hello,world);
+
+
+    }
+}
