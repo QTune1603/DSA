3c897f68afb6c79c93973b33572ee2a54cb6a688
QiTunggg <128139863+QiTunggg@users.noreply.github.com>
2023-12-28 08:41:33 +0700

    Update BTThucHanh5.java

--- BTThucHanh5.java
+++ BTThucHanh5.java
@@ -8,23 +8,42 @@ public class BTThucHanh5 {
         int n = sc.nextInt();
 
         /*
-           *
-          ***
-         *****
-        *******
-       *********
+            *
+           ***
+          *****
+         *******
+        *********
          */
 
-        // for(int i = 1 ; i <= n ; i++){
-        // for(int j = 1 ; j <= n-i ; j++){
-        // System.out.print(" ");
-        // }
-        // for(int j = 1 ; j <= 2*i-1 ; j++){
-        // System.out.print("*");
-        // }
-        // System.out.println();
-        // }
-        // sc.close();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        /*
+         *********
+          *******
+           *****
+            ***
+             *
+         */
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
 
         /*
             *
@@ -33,12 +52,13 @@ public class BTThucHanh5 {
             ****
             *****
         */
-        // for(int i = n ; i >= 0 ; i--){
-        // for(int j = 1 ; j <= n-i ; j++){
-        // System.out.print("*");
-        // }
-        // System.out.println();
-        // }
+        for (int i = n; i >= 0; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
 
         /*
             *****
@@ -46,14 +66,15 @@ public class BTThucHanh5 {
             ***
             **
             *
-
+        
         */
-        // for (int i = 1; i <= n; i++) {
-        //     for (int j = 1; j <= n - i + 1; j++) {
-        //         System.out.print("*");
-        //     }
-        //     System.out.println();
-        // }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
 
         /*
              *
@@ -63,27 +84,30 @@ public class BTThucHanh5 {
          *****
          */
 
-        // for(int i = 1 ; i <= n ; i++){
-        //     for(int j = n ; j >= 0 ; j--){
-        //         if(i <= j) System.out.print(" ");
-        //         else System.out.print("*");
-        //     }
-        //     System.out.println();
-        // }
-
-        /*
-         *****
-          ****
-           ***
-            **
-             *
-         */
-        for(int i = 1 ; i <= n ; i++){
-            for(int j = 1 ; j <= n ; j++){
-                if(j < i){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n ; j++) {
+                if (j <= n-i)
                     System.out.print(" ");
-                }
-                else System.out.print("*");
+                else
+                    System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        /*
+         *****
+          ****
+           ***
+            **
+             *
+         */
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (j < i) {
+                    System.out.print(" ");
+                } else
+                    System.out.print("*");
             }
             System.out.println();
         }
